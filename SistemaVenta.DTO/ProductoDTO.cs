9759fdfd267a70322fc06dcfc147e929ef6dfb9b
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2025-04-21 15:31:16 -0500

    feat: implement DTO layer with all domain models

---
+++ SistemaVenta.DTO/ProductoDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.DTO
+{
+    public class ProductoDTO
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; } = null!;
+        public int? IdCategoria { get; set; }
+        public string? DescripcionCategoria { get; set; } //específicamente para mostrar la descripción o nombre de la categoría
+        public int? IdProveedor { get; set; }
+        public string? NombreProveedor { get; set; } //específicamente para mostrar el nombre del proveedor
+        public int? IdImagen { get; set; }
+        public string? NombreImagen { get; set; } // Nombre del archivo de imagen
+        public string? RutaImagen { get; set; } // Ruta de la imagen en el servidor
+        public int Stock { get; set; }             
+        public int? EsActivo { get; set; }//lo cambiamos de boolen a entero 
+    }
+}
---
+++ SistemaVenta.DTO/ProductoEmpaqueDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.DTO
+{
+    public class ProductoEmpaqueDTO
+    {
+        public int IdProductoEmpaque { get; set; }
+        public int IdProducto { get; set; }
+        public string? DescripcionProducto { get; set; } //específicamente para mostrar la descripción o nombre del producto
+        public int IdPresentacion { get; set; }
+        public string? DescripcionPresentacion { get; set; } //específicamente para mostrar la descripción o nombre del presentación
+        public int? Cantidad { get; set; }
+        public string? PrecioVenta { get; set; }//lo cambiamos de decimal a string
+        public string? PrecioCompra { get; set; }//lo cambiamos de decimal a string
+        public int? EsActivo { get; set; }//lo cambiamos de boolen a entero 
+    }
+}
